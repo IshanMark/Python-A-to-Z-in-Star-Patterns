ddb160e7a9348b8e0fe224875e6851927477dfb9
IshanMark <rupasing-cs18036@stu.kln.ac.lk>
2022-10-22 10:50:04 +0530

    Add H

--- A-to-Z pattern.py
+++ A-to-Z pattern.py
@@ -79,6 +79,7 @@ print(str)
 
 print("\n")
 
+
 # print G
 str = ""
 for row in range(7):
@@ -92,11 +93,11 @@ print(str)
 
 print("\n")
 
-# print G
+# print H
 str = ""
 for row in range(7):
     for col in range(8):
-        if((col ==1 and row!=0 and row!=6) or((row==0 or row==6) and col>1 and col<5) or (row==3 and col<6 and col>2) or (col==7 and row!=0 and row !=2 and row!=6 and row != 3)):
+        if((col ==1 or col == 6 and row!=3) or (row == 3 and col<5 and col>1 )):
             str = str+"* "
         else:
             str = str+" "
