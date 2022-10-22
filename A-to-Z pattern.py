4d813ce626f484a57987d73ce32a4fe3b0ba0688
IshanMark <rupasing-cs18036@stu.kln.ac.lk>
2022-10-22 11:06:36 +0530

    Add I

--- A-to-Z pattern.py
+++ A-to-Z pattern.py
@@ -105,3 +105,16 @@ for row in range(7):
 print(str)
 
 print("\n")
+
+# print I
+str = ""
+for row in range(7):
+    for col in range(5):
+        if((row ==1 or row == 6 ) or (col == 4 and row<6 and row>1 )):
+            str = str+"* "
+        else:
+            str = str+" "
+    str = str+"\n"
+print(str)
+
+print("\n")
