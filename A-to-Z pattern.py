b42177c09c9d2c9e7761e97f6d58a2c6c4ca9094
IshanMark <rupasing-cs18036@stu.kln.ac.lk>
2022-10-22 23:33:09 +0530

    Add P

--- A-to-Z pattern.py
+++ A-to-Z pattern.py
@@ -213,12 +213,25 @@ print("\n")
 str = ""
 for row in range(7):
     for col in range(8):
+        if(col==1 or ((row==0 or row==3) and (col<5 and col>1))or (col == 7  and (row!=0 and row!=3 and row<4))):
+            str = str+"* "
+        else:
+            str = str+" "
+    str = str+"\n"
+print(str)
+print("\n")
+
+# print R
+str = ""
+for row in range(7):
+    for col in range(8):
         if(col==1 or ((row==0 or row==3) and (col<5 and col>1))or (col == 7  and (row!=0 and row!=3))):
             str = str+"* "
         else:
             str = str+" "
     str = str+"\n"
 print(str)
+print("\n")
 
 
 
